e4685bf0e9ab62b807816b938a9fa7eebbe44aa4
Charles Magalhães <charles.magalhaes.developer@gmail.com>
2025-09-09 21:00:08 -0300

    Add files via upload

---
+++ PrjNaviosEContainers/src/main/java/com/poo/prjnaviosecontainers/objetos/container.java
@@ -0,0 +1,16 @@
+package com.poo.prjnaviosecontainers.objetos;
+
+/**
+ *
+ * @author iftm
+ */
+public class container {
+    private String proprietario;
+    private String numIso;
+    private String tipo;
+    private String conteudo;
+    private double peso;
+    private String dataEntregaPorto;
+    private String dataSaidaPorto;
+    private navio_porta_container navio;
+}
---
+++ PrjNaviosEContainers/src/main/java/com/poo/prjnaviosecontainers/objetos/navio_porta_container.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poo.prjnaviosecontainers.objetos;
+
+/**
+ *
+ * @author iftm
+ */
+public class navio_porta_container {
+    
+}
